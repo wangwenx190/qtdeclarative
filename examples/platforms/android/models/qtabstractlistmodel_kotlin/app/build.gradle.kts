7715c5f056a4c8201bd3a638defd8b9980cb6b64
Inkamari Harjula <inkamari.harjula@qt.io>
2024-11-06 13:57:19 +0200

    Fix String '6.8.0' found in 6.9.0 sources in qtdeclarative
    Occurrences fixed.
    
    Task-number: QTBUG-130833
    Change-Id: Ideda05852ce9bbfbce3dfa7bdf7c06155c598f46
    Reviewed-by: Mats Honkamaa <mats.honkamaa@qt.io>

--- examples/platforms/android/models/qtabstractlistmodel_kotlin/app/build.gradle.kts
+++ examples/platforms/android/models/qtabstractlistmodel_kotlin/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 //! [build.gradle QtBuild config]
 QtBuild {
     // Relative for Qt (Installer or MaintenanceTool) installations.
-    qtPath = file("../../../../../../../6.8.0")
+    qtPath = file("../../../../../../../6.9.0")
     projectPath = file("../../qtabstractlistmodel")
 }
 //! [build.gradle QtBuild config]
