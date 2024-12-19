735556d19d7bd67a1a9729586743e1c8b86de257
Ulf Hermann <ulf.hermann@qt.io>
2024-12-19 14:52:09 +0100

    QtQml: Accept .js and .mjs files in directory-listing qmldirs
    Since it's documented like this, we should actually allow it.
    
    Pick-to: 6.9 6.8 6.5
    Task-number: QTBUG-132118
    Change-Id: I2192f040b8fdf545b8dbb0687b6e618a9858ed07
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>

--- src/qml/qmldirparser/qqmldirparser_p.h
+++ src/qml/qmldirparser/qqmldirparser_p.h
@@ -141,6 +141,8 @@ private:
     bool maybeAddComponent(const QString &typeName, const QString &fileName, const QString &version, QHash<QString,Component> &hash, int lineNumber = -1, bool multi = true);
     void reportError(quint16 line, quint16 column, const QString &message);
     QString scanQuotedWord(const QChar *&ch, quint16 lineNumber, quint16 columnNumber);
+    void insertComponentOrScript(
+            const QString &name, const QString &fileName, QTypeRevision version);
 
 private:
     QList<QQmlJS::DiagnosticMessage> _errors;
--- tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
+++ tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
@@ -30,6 +30,7 @@
 using namespace Qt::StringLiterals;
 
 Q_IMPORT_QML_PLUGIN(TestTypesPlugin)
+Q_IMPORT_QML_PLUGIN(ConfusedPlugin)
 
 class tst_QmlCppCodegen : public QObject
 {
@@ -71,6 +72,7 @@ private slots:
     void componentReturnType();
     void compositeSingleton();
     void compositeTypeMethod();
+    void confusedModule();
     void consoleObject();
     void consoleTrace();
     void construct();
@@ -1109,6 +1111,16 @@ void tst_QmlCppCodegen::compositeTypeMethod()
     QVERIFY(!object.isNull());
     QSignalSpy spy(object.data(), SIGNAL(foo()));
     QTRY_VERIFY(spy.size() > 0);
+}
+
+void tst_QmlCppCodegen::confusedModule()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine, QUrl(u"qrc:/qt/qml/Confused/Main.qml"_s));
+    QVERIFY2(!component.isError(), component.errorString().toUtf8());
+    QTest::ignoreMessage(QtDebugMsg, "Hello from Test");
+    QScopedPointer<QObject> object(component.create());
+    QVERIFY(!object.isNull());
 }
 
 void tst_QmlCppCodegen::consoleObject()
