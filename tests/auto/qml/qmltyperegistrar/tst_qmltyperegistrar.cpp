319aa02fe2bab08c2c91eb504237839d1448bf73
Ulf Hermann <ulf.hermann@qt.io>
2025-01-13 15:01:11 +0100

    qmltyperegistrar: Correctly handle namespaced foreign objects
    The findType() may determine that the target object is in a namespace.
    Use the found type's qualified name rather than the name given in the
    QML_FOREIGN declaration.
    
    Pick-to: 6.9 6.8
    Fixes: QTBUG-132100
    Change-Id: I1e8023a04fced64a5e6cd198ba7651712ee011e4
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>

--- tests/auto/qml/qmltyperegistrar/tst_qmltyperegistrar.cpp
+++ tests/auto/qml/qmltyperegistrar/tst_qmltyperegistrar.cpp
@@ -1267,6 +1267,25 @@ void tst_qmltyperegistrar::derivedFromInvisible()
     })"));
 }
 
+void tst_qmltyperegistrar::foreignNamespacedWithEnum()
+{
+    QVERIFY(qmltypesData.contains(R"(Component {
+        file: "tst_qmltyperegistrar.h"
+        name: "F::ForeignQObject"
+        accessSemantics: "reference"
+        prototype: "QObject"
+        exports: ["QmlTypeRegistrarTest/ForeignQObject 1.0"]
+        isCreatable: false
+        exportMetaObjectRevisions: [256]
+        Enum {
+            name: "Enum"
+            isScoped: true
+            values: ["ValueA", "ValueB"]
+        }
+    })"));
+}
+
+
 #ifdef QT_QMLJSROOTGEN_PRESENT
 void tst_qmltyperegistrar::verifyJsRoot()
 {
@@ -1310,6 +1329,7 @@ void tst_qmltyperegistrar::verifyJsRoot()
         QCOMPARE(currentLines[i], generatedLines[i]);
     }
 }
+
 #endif
 
 QTEST_MAIN(tst_qmltyperegistrar)
